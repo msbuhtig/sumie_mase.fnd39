79ccc554d7fb6633c7066d208742db59b950e160
sumie mase <163987721+msbuhtig@users.noreply.github.com>
2025-09-03 16:19:23 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,17 +1,10 @@
 'use strict'
 // 1行目に記載している 'use strict' は削除しないでください
 
-// const words = [
-//   { word: "apple", meaning: "りんご" },
-//   { word: "banana", meaning: "バナナ" },
-//   { word: "cat", meaning: "猫" },
-//   { word: "dog", meaning: "犬" },
-//   { word: "elephant", meaning: "象" }
-// ];
-
 //const dataURL = "http://127.0.0.1:5500/Assignment_FlashCard/submission/data/data.json";
 const dataURL = "https://raw.githubusercontent.com/msbuhtig/sumie_mase.fnd39/main/data/data.json";
 const numQuestions = 3;
+const imgMain = "images/jungle_track.png";
 const imgEnding = "images/ending.jpeg";
 const imgBadEnding = "images/badending.jfif";
 const imgCollect = "images/btn_correct.jfif";
@@ -27,11 +20,7 @@ const gameStart = document.getElementById("start");
 const main = document.getElementById("main");
 const topic = document.getElementById("topic");
 const leftCard = document.getElementById("left-card");
-const leftWord = document.getElementById("left-word");
-const leftAnswer = document.getElementById("left-answer");
 const rightCard = document.getElementById("right-card");
-const rightWord = document.getElementById("right-word");
-const rightAnswer = document.getElementById("right-answer");
 const ending = document.getElementById("ending");
 const nextGame = document.getElementById("btnNextGame");
 
@@ -64,7 +53,7 @@ const checkFinal = function () {
 }
 
 const postProc = function () {
-  main.remove();
+  main.style.display = "none";
   document.body.style.backgroundImage = `url('${imgEnding}')`;
   ending.style.display = "block";
 //  ending.textContent = "おめでとうござます！ 全問正解です";
@@ -97,12 +86,12 @@ const setCard = function () {
   setIndex();
   topic.textContent = words[currentIdx].topic;
   if (Math.round(Math.random()) === 0) {
-    leftWord.textContent = words[currentIdx].meaning;
-    rightWord.textContent = words[incollectIdx].meaning;
+    leftCard.textContent = words[currentIdx].meaning;
+    rightCard.textContent = words[incollectIdx].meaning;
     isLeftCollect = true;
   } else {
-    leftWord.textContent = words[incollectIdx].meaning;;
-    rightWord.textContent = words[currentIdx].meaning;
+    leftCard.textContent = words[incollectIdx].meaning;;
+    rightCard.textContent = words[currentIdx].meaning;
     isLeftCollect = false;
   }}
 
@@ -117,7 +106,7 @@ const showNextTopic = function () {
 };
 
 const showBadEnding = function () {
-    main.remove();
+    main.style.display = "none";
     document.body.style.backgroundImage = `url('${imgBadEnding}')`;
     ending.style.display = "block";
 };
@@ -129,25 +118,25 @@ document.addEventListener("DOMContentLoaded", () => {
 leftCard.addEventListener("click", () => {
   const prevIdx = currentIdx;
   if (isLeftCollect) {
-    leftWord.textContent = "";
+    leftCard.textContent = "";
     leftCard.style.backgroundImage = `url('${imgCollect}')`;
     const timer = setTimeout(showNextTopic, 3000);
     if (prevIdx !== currentIdx) clearTimeout(timer);
   } else {
-    leftWord.textContent = "";
+    leftCard.textContent = "";
     leftCard.style.backgroundImage = `url('${imgIncollect}')`;
     const timer = setTimeout(showBadEnding, 1000);
   }
 });
 
-rightCard.addEventListener("click", () => { // 右のカードをクリックしたら
+rightCard.addEventListener("click", () => {
   const prevIdx = currentIdx;
-  if (isLeftCollect) {  // 左のカードが正解だったら
-    rightWord.textContent = "";
-    rightCard.style.backgroundImage = `url('${imgIncollect}')`; // 右のカードに 失敗 の画像を表示
+  if (isLeftCollect) {
+    rightCard.textContent = "";
+    rightCard.style.backgroundImage = `url('${imgIncollect}')`;
     const timer = setTimeout(showBadEnding, 1000);
   } else {
-    rightWord.textContent = "";
+    rightCard.textContent = "";
     rightCard.style.backgroundImage = `url('${imgCollect}')`;
     const timer = setTimeout(showNextTopic, 3000);
     if (prevIdx !== currentIdx) clearTimeout(timer);
@@ -155,11 +144,9 @@ rightCard.addEventListener("click", () => { // 右のカードをクリックし
 });
 
 gameStart.addEventListener("click", () => {
-  gameStart.remove();
+  gameStart.style.display = "none";
   main.style.display = "block";
   setCard();
-  leftWord.style.display = "block";
-  rightWord.style.display = "block";
 });
 
 nextGame.addEventListener("click", () => {
